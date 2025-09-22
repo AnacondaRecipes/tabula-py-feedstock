f9d944dac71b55ef2e31cd3c78114236b65a3261
Patryk Kups <pkups@anaconda.com>
2025-09-22 19:57:47 +0200

    check

--- recipe/run_test.py
+++
@@ -1,7 +0,0 @@
-import os
-import tabula
-
-# Windows automated builds are failing.. disabling the test
-if 'windir' not in os.environ:
-    # Read remote pdf into DataFrame
-    df2 = tabula.read_pdf("https://github.com/tabulapdf/tabula-java/raw/master/src/test/resources/technology/tabula/arabic.pdf")
